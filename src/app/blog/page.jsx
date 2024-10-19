725ec3f97859faaa5a0a5976d31b236cb5ab1aa0
ZhubinR <zhubinzed@gmail.com>
2024-10-19 15:16:02 +0330

    blog edits

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -54,17 +54,6 @@ const BlogArchive = async () => {
 
       <div className="container">
         <div className="row align-items-center justify-content-center pt-4">
-          <div key={61985} className="col-lg-4 col-md-6 mb-4">
-            <BlogItem
-              slug={`/blog/national-doctors-day`}
-              imageUrl={`/images/natinal-dr-thumbnail.webp`}
-              title={`روز پزشک مبارک`}
-              description={`روز پزشک که مصادف با اول شهریور ماه و سالروز تولد بوعلی سینا، بزرگ‌ترین پزشک و دانشمند ایرانی است، فرصتی است برای تجلیل از خدمات ارزنده‌ی پزشکان و تقدیر از تلاش‌های بی‌وقفه‌ی آنان در راستای حفظ و ارتقاء سلامت جامعه.`}
-              updatedAt={`1403/05/24`}
-              readingTime={`8 دقیقه`}
-              tag={`سایر`}
-            />
-          </div>
           {blogPostsWithCategories.map((post) => (
             <div key={post.id} className="col-lg-4 col-md-6 mb-4">
               <BlogItem
@@ -78,6 +67,17 @@ const BlogArchive = async () => {
               />
             </div>
           ))}
+          <div key={61985} className="col-lg-4 col-md-6 mb-4">
+            <BlogItem
+              slug={`/blog/national-doctors-day`}
+              imageUrl={`/images/natinal-dr-thumbnail.webp`}
+              title={`روز پزشک مبارک`}
+              description={`روز پزشک که مصادف با اول شهریور ماه و سالروز تولد بوعلی سینا، بزرگ‌ترین پزشک و دانشمند ایرانی است، فرصتی است برای تجلیل از خدمات ارزنده‌ی پزشکان و تقدیر از تلاش‌های بی‌وقفه‌ی آنان در راستای حفظ و ارتقاء سلامت جامعه.`}
+              updatedAt={`1403/05/24`}
+              readingTime={`8 دقیقه`}
+              tag={`سایر`}
+            />
+          </div>
         </div>
       </div>
     </main>
